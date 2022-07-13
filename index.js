5e8d93d72ebb2d2c332636064240ee27ed069718
Alessandro Diana <69928507+AleD1996@users.noreply.github.com>
2022-07-14 01:07:48 +0200

    Add js and css for the index page

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("qui funza, daje!")
+
+let unaVariabile = document.getElementById("buttonDaje")
+
+unaVariabile.addEventListener("click", ()=>{
+	unaVariabile.style.backgroundColor = "blue"
+})
