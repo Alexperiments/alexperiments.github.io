353f895dee04acfb18cf31a39d9cd61fc358fdb6
Alessandro Diana <69928507+AleD1996@users.noreply.github.com>
2022-07-27 12:18:07 +0200

    Add a coming soon template

--- index.js
+++
@@ -1,7 +0,0 @@
-console.log("qui funza, daje!")
-
-let unaVariabile = document.getElementById("buttonDaje")
-
-unaVariabile.addEventListener("click", ()=>{
-	unaVariabile.style.backgroundColor = "blue"
-})
